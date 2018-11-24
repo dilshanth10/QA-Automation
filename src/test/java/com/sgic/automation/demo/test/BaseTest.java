80aa2dcdf44c4eab3daa75fb30d8ed2a92595f47
Dilshanth <dilshanth10@gmail.com>
2018-11-24 16:16:29 +0530

    screenshot and excel import

--- src/test/java/com/sgic/automation/demo/test/BaseTest.java
+++ src/test/java/com/sgic/automation/demo/test/BaseTest.java
@@ -1,12 +1,19 @@
 package com.sgic.automation.demo.test;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.sgic.automation.demo.util.Functions;
 
 public class BaseTest {
 //String driverPathChrome = "C:\\Users\\Dilshanth\\Documents\\GitHub\\eclipse\\automation\\webdrivers\\chromedriver.exe";
@@ -18,7 +25,15 @@ public class BaseTest {
   private StringBuffer verificationErrors = new StringBuffer();
   
   protected static Properties prop = new Properties();
+  
+//  private static String screenShotFolderPath = "C:\\Users\\Dilshanth\\Documents\\GitHub\\eclipse\\automation\\src\\test\\resources\\screenshots";
+  private static String screenShotFolderPath = System.getProperty("user.dir") + "\\src\\test\\resources\\screenshots\\";
 
+  public static ExtentReports extentReport = new ExtentReports(System.getProperty("user.dir", "\\src\\test\\resources\\results"));
+  public static ExtentTest extentTest;
+  
+  public static String excelFilePath = "\\src\\test\\resources\\data\\credentials.xlsx";
+  
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     baseUrl = "https://www.katalon.com/";
@@ -57,8 +72,22 @@ public class BaseTest {
 //    driver = new FirefoxDriver();
   }
   
+  public static String captureScreeShot() {
+    String filename= Functions.getTimeStamp("yyyy-MM-dd_HH:mm:ss")+".jpg";
+    File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    try {
+      FileUtils.copyFile(screenshotFile, new File("e:\\screenshot\\screenshot.png"));
+    }
+    catch (Exception e) {
+      // TODO: handle exception
+    }
+    
+    return filename;
+  }
+  
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
+    extentReport.flush();
     driver.quit();
   }
 
--- src/test/java/com/sgic/automation/demo/test/LoginTest.java
+++ src/test/java/com/sgic/automation/demo/test/LoginTest.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.LogStatus;
+import com.sgic.automation.demo.util.DataStore;
 import com.sgic.automation.demo.util.Log;
 
 public class LoginTest extends BaseTest{
@@ -17,27 +19,72 @@ public class LoginTest extends BaseTest{
   @Test
   public void testOrangeHRM() throws Exception {
 //    driver.get("https://opensource-demo.orangehrmlive.com/");
-    driver.get(prop.getProperty("baseUrl"));
-    Log.startTestCase();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='LOGIN Panel'])[1]/following::span[1]")).click();
-    driver.findElement(By.id("txtUsername")).clear();
-    driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-    logger.log(Level.INFO, "********* Entered username **********");
-    driver.findElement(By.id("txtPassword")).clear();
-    driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-    logger.log(Level.INFO, "********* Entered password **********");
-    driver.findElement(By.id("btnLogin")).click();
-    logger.log(Level.INFO, "********* Login button clicked **********");
-    Thread.sleep(2000);
-    driver.findElement(By.id("welcome")).click();
-    Thread.sleep(2000);
-    try {
-      assertEquals(driver.findElement(By.id("welcome")).getText(), "Welcome Admin");
-    } catch (Error e) {
-      logger.log(Level.ERROR, "Failed to find mesasge");
+    
+    DataStore.readExcelFile(excelFilePath);
+    DataStore.loadData();
+    for(int i=0; i<DataStore.testData.get("username").size(); i++) {
+      extentTest = extentReport.startTest("Admin Login");
+      driver.get(prop.getProperty("baseUrl"));
+      Log.startTestCase();
+      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='LOGIN Panel'])[1]/following::span[1]")).click();
+      driver.findElement(By.id("txtUsername")).clear();
+      driver.findElement(By.id("txtUsername")).sendKeys(DataStore.testData.get("username").get(i));
+      extentTest.log(LogStatus.PASS, "Entered username");
+      logger.log(Level.INFO, "********* Entered username **********");
+      driver.findElement(By.id("txtPassword")).clear();
+      driver.findElement(By.id("txtPassword")).sendKeys(DataStore.testData.get("password").get(i));
+      extentTest.log(LogStatus.PASS, "Entered password");
+      logger.log(Level.INFO, "********* Entered password **********");
+      driver.findElement(By.id("btnLogin")).click();
+      extentTest.log(LogStatus.PASS, "Login button clicked");
+      logger.log(Level.INFO, "********* Login button clicked **********");
+      Thread.sleep(2000);
+      driver.findElement(By.id("welcome")).click();
+      Thread.sleep(2000);
+      try {
+        assertEquals(driver.findElement(By.id("welcome")).getText(), "Welcomex Admin");
+        logger.log(Level.INFO,"Verified Welcome Message");
+      } catch (Error e) {
+        String file = captureScreeShot();
+        extentTest.log(LogStatus.FAIL, "Failed to find the message");
+        logger.log(Level.ERROR, "Failed to find mesasge. \n" + "Screenshot file: " + file);
+      }
+      driver.findElement(By.linkText("Logout")).click();
+      Log.endTestCase();
+      extentReport.endTest(extentTest);
     }
-    driver.findElement(By.linkText("Logout")).click();
-    Log.endTestCase();
+    
+    
+    
+//    extentTest = extentReport.startTest("Admin Login");
+//    driver.get(prop.getProperty("baseUrl"));
+//    Log.startTestCase();
+//    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='LOGIN Panel'])[1]/following::span[1]")).click();
+//    driver.findElement(By.id("txtUsername")).clear();
+//    driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+//    extentTest.log(LogStatus.PASS, "Entered username");
+//    logger.log(Level.INFO, "********* Entered username **********");
+//    driver.findElement(By.id("txtPassword")).clear();
+//    driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+//    extentTest.log(LogStatus.PASS, "Entered password");
+//    logger.log(Level.INFO, "********* Entered password **********");
+//    driver.findElement(By.id("btnLogin")).click();
+//    extentTest.log(LogStatus.PASS, "Login button clicked");
+//    logger.log(Level.INFO, "********* Login button clicked **********");
+//    Thread.sleep(2000);
+//    driver.findElement(By.id("welcome")).click();
+//    Thread.sleep(2000);
+//    try {
+//      assertEquals(driver.findElement(By.id("welcome")).getText(), "Welcomex Admin");
+//      logger.log(Level.INFO,"Verified Welcome Message");
+//    } catch (Error e) {
+//      String file = captureScreeShot();
+//      extentTest.log(LogStatus.FAIL, "Failed to find the message");
+//      logger.log(Level.ERROR, "Failed to find mesasge. \n" + "Screenshot file: " + file);
+//    }
+//    driver.findElement(By.linkText("Logout")).click();
+//    Log.endTestCase();
+//    extentReport.endTest(extentTest);
   }
   
 }
